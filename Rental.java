509b2258238306590727c2d96f47505c654bc908
sima golrokh bahoush <sima.golrokh@gmail.com>
2025-04-18 19:47:09 +0200

    add related method getcharge to Rental class

--- Rental.java
+++ Rental.java
@@ -1,6 +1,6 @@
 class Rental {
-    private Movie movie;
-    private int daysRented;
+    private final Movie movie;
+    private final int daysRented;
     public Rental(Movie newmovie, int newdaysRented) {
         movie = newmovie;
         daysRented = newdaysRented;
@@ -11,4 +11,23 @@ class Rental {
     public Movie getMovie() {
         return movie;
     }
+    public double getCharge() {
+        return switch (movie.getPriceCode()) {
+            case Movie.REGULAR -> {
+                double amount = 2;
+                if (daysRented > 2)
+                    amount += (daysRented - 2) * 1.5;
+                yield amount;
+            }
+            case Movie.NEW_RELEASE -> daysRented * 3;
+            case Movie.CHILDRENS -> {
+                double amount = 1.5;
+                if (daysRented > 3)
+                    amount += (daysRented - 3) * 1.5;
+                yield amount;
+            }
+            default -> throw new IllegalArgumentException("Invalid price code: " + movie.getPriceCode());
+        };
+    }
+    
 }
